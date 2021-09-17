2efebfd52a1077ecc10d7251f81cefa8dc87e8e9
Anatoly Lisinchyk <anatoliy.lis@bmby.com>
2021-09-17 12:45:26 +0300

    config env, logic generate signature

--- index.js
+++ index.js
@@ -1,76 +1,95 @@
 const express = require("express");
-const cors = require("cors"); //Cross-Origin Resource Sharing
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
-
-//include required modules
+require('dotenv').config();
 const jwt = require("jsonwebtoken");
-const config = require("./config");
 const rp = require("request-promise");
+const crypto = require('crypto');
 
 const app = express();
+
 const PORT = process.env.PORT || 3444;
+const APIKey = process.env.APIKey;
+const APISecret = process.env.APISecret;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
     limit: "50mb",
     extended: true,
-  })
-);
+}));
+
 app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express and Zoom API" });
+    response.json({info: "Node.js, Express and Zoom API"});
 });
 
-//Use the ApiKey and APISecret from config.js
-const payload = {
-  iss: config.APIKey,
-  exp: new Date().getTime() + 5000,
-};
 
-const token = jwt.sign(payload, config.APISecret);
 app.post("/zoomcall", (req, res) => {
-  const email = req.body.email;
-  const options = {
-    method: "POST",
-    uri: "https://api.zoom.us/v2/users/" + email + "/meetings",
-    body: {
-      topic: "Meeting",
-      type: 1,
-      settings: {
-        host_video: "true",
-        participant_video: "true",
-      },
-    },
-    auth: {
-      bearer: token,
-    },
-    headers: {
-      "User-Agent": "Zoom-api-Jwt-Request",
-      "content-type": "application/json",
-    },
-    json: true, //Parse the JSON string in the response
-  };
+    const {email, userName} = req.body;
 
-  rp(options)
-    .then(function (response) {
-      console.log("response is: ", response.join_url);
-      // response.status(200).json(response);
-      let dataRes = {
-        join_url: response.join_url,
-      };
-      res.status(200).json(dataRes);
+    const token = jwt.sign({
+        iss: APIKey,
+        exp: new Date().getTime() + 5000,
+    }, APISecret);
 
-      // res.send("create meeting result: " + JSON.stringify(response));
-    })
-    .catch(function (err) {
-      // API call failed...
-      res.status(500).json(err.message);
-      console.log("API call failed, reason ", err);
-    });
+    const options = {
+        method: "POST",
+        uri: "https://api.zoom.us/v2/users/" + email + "/meetings",
+        body: {
+            topic: "Meeting",
+            type: 1,
+            settings: {
+                host_video: "true",
+                participant_video: "true",
+            },
+        },
+        auth: {
+            bearer: token,
+        },
+        headers: {
+            "User-Agent": "Zoom-api-Jwt-Request",
+            "content-type": "application/json",
+        },
+        json: true, //Parse the JSON string in the response
+    };
+
+    rp(options)
+        .then(function (response) {
+            const info = response.join_url.replaceAll('https://us04web.zoom.us/j/', '').split('?');
+            const meetingNumber = +info[0];
+            const password = info[1].replaceAll('pwd=', '');
+
+            const signature = generateSignature(APIKey, APISecret, meetingNumber, 1);
+
+            const data = {
+                name: userName,
+                mn: meetingNumber,
+                pwd: password,
+                role: 1,
+                email,
+                lang: "en_US",
+                signature,
+                china: 0,
+                APIKey
+            }
+
+            res.status(200).json(data);
+        })
+        .catch(function (err) {
+            res.status(500).json(err.message);
+            console.log("API call failed, reason ", err);
+        });
 });
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}.`);
+    console.log(`App running on port ${PORT}.`);
 });
+
+function generateSignature(apiKey, apiSecret, meetingNumber, role) {
+    const timestamp = new Date().getTime() - 30000;
+    const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString('base64');
+    const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64');
+    const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64');
+
+    return signature;
+}
